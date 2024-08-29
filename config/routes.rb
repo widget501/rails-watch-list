8ab93a31e99078fc12b763341189a0b1d2534071
JP Breugem <36500401+widget501@users.noreply.github.com>
2024-08-29 16:24:37 +0200

    Upd rails watch list

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: "lists#index"
+  resources :lists, except: [ :edit, :update ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :bookmarks, only: :destroy
 end
